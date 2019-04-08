235e25ae56f80c828902799fae1740711c9a8434
tfedi <49347046+tfedi@users.noreply.github.com>
2019-04-08 18:42:08 -0300

    SACA LA MANO DE AHI CARAJO

--- newton.py
+++ newton.py
@@ -1,7 +1,5 @@
 import random
 import math
-#Newton-Rhapson Method:
-#Xn+1 = Xn - f(Xn)/f'(Xn)
 
 def ex(x):
     return math.cos(x) - x**3
@@ -22,20 +20,3 @@ def newtonMethod(f,Xn,k):
         return Xn_1
     else:
         return newtonMethod(f,Xn_1,k-1)
-
-# def searchRange(f,a,b,t):
-#     """Find an existing range for a root between a and b, using delta t, and returns c."""
-#     if f(b) > 0 and f(a) < 0:
-#         x1 = f(a)
-#         x2 = f(b)
-#     elif f(a) > 0 and f(b) < 0:
-#         x1 = f(b)
-#         x2 = f(a)
-#     else:
-#         print("\nSince f(a) and f(b) have the same sign, it can not be decided if Bolzano's theorem is valid.\n")
-#         return False
-#     #X1 < X2 (X1 NEGATIVE, X2 POSITIVE)
-#     c = random.randint(a,b)
-#     while not(f(c) <= (0.1) and f(c) >= (-0.1)):
-#         x1 =
-#     return c
